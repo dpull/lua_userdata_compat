6a5b53605d6714b304349e7a5c9b586132f65a47
dpull <beingstudio@gmail.com>
2022-08-01 12:02:48 +0800

    define bug

--- lua_userdata_compat.h
+++ lua_userdata_compat.h
@@ -4,6 +4,6 @@
 
 #if LUA_VERSION_NUM < 504
 LUA_API void *(lua_newuserdatauv)(lua_State *L, size_t sz, int nuvalue);
-LUA_API int(lua_getmetatable)(lua_State *L, int objindex);
 LUA_API int(lua_getiuservalue)(lua_State *L, int idx, int n);
+LUA_API int(lua_setiuservalue)(lua_State *L, int idx, int n);
 #endif
